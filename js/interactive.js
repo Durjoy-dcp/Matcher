de1f794aaa996426558435a67f7024e53e57d50d
Durjoy-dcp <unknownccxv@gmail.com>
2022-08-19 12:50:03 +0600

    clicked button of calculator

--- js/interactive.js
+++ js/interactive.js
@@ -16,3 +16,21 @@ document.getElementById('generator').addEventListener('click', function () {
     document.getElementById('numberShow').value = number;
 
 })
+document.getElementById('calculator').addEventListener('click', function (event) {
+    const buttonPresse = event.target.innerText;
+    let displayNumber = document.getElementById('display-number');
+    console.log(displayNumber);
+    if (isNaN(buttonPresse)) {
+        if (buttonPresse === 'C') {
+            displayNumber.value = '';
+        }
+        if (buttonPresse === '<') {
+            displayNumber.value = displayNumber.value.substring(0, displayNumber.value.length - 1);
+
+        }
+        console.log(buttonPresse);
+    } else {
+        displayNumber.value += buttonPresse;
+
+    }
+})
